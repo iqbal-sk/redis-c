7c6e33b0d53edf94a74cae2997e4f2bb3e16038e
Iqbal <mahammad@buffalo.edu>
2025-10-16 17:49:02 -0400

    refactor: Comment out unused REPLCONF and PSYNC commands in server_connect_master

--- src/server.c
+++ src/server.c
@@ -260,32 +260,32 @@ int server_connect_master(Server *srv, const char *host, int port)
         // Keep connection open; later stages may retry
     }
     // Send REPLCONF listening-port <port>
-    char rp1[128];
-    int l1 = snprintf(rp1, sizeof(rp1),
-                      "*3\r\n$8\r\nREPLCONF\r\n$14\r\nlistening-port\r\n$%d\r\n%d\r\n",
-                      (int)snprintf(NULL, 0, "%d", srv->listen_port), srv->listen_port);
-    if (l1 > 0 && (size_t)l1 < sizeof(rp1))
-    {
-        if (send_all(fd, rp1, (size_t)l1) != 0)
-            printf("replica connect: send REPLCONF listening-port failed (%s)\n", strerror(errno));
-    }
-    // Send REPLCONF capa psync2
-    const char *rp2 = "*3\r\n$8\r\nREPLCONF\r\n$4\r\ncapa\r\n$6\r\npsync2\r\n";
-    if (send_all(fd, rp2, strlen(rp2)) != 0)
-    {
-        printf("replica connect: send REPLCONF capa failed (%s)\n", strerror(errno));
-    }
-    // Send PSYNC ? -1
-    // const char *ps = "*3\r\n$5\r\nPSYNC\r\n$1\r\n?\r\n$2\r\n-1\r\n";
-    // if (send_all(fd, ps, strlen(ps)) != 0)
+    // char rp1[128];
+    // int l1 = snprintf(rp1, sizeof(rp1),
+    //                   "*3\r\n$8\r\nREPLCONF\r\n$14\r\nlistening-port\r\n$%d\r\n%d\r\n",
+    //                   (int)snprintf(NULL, 0, "%d", srv->listen_port), srv->listen_port);
+    // if (l1 > 0 && (size_t)l1 < sizeof(rp1))
     // {
-    //     printf("replica connect: send PSYNC failed (%s)\n", strerror(errno));
+    //     if (send_all(fd, rp1, (size_t)l1) != 0)
+    //         printf("replica connect: send REPLCONF listening-port failed (%s)\n", strerror(errno));
     // }
-    // Store master coordinates on server for later stages
-    strncpy(srv->master_host, host, sizeof(srv->master_host) - 1);
-    srv->master_host[sizeof(srv->master_host) - 1] = '\0';
-    srv->master_port = port;
-    return 0;
+    // // Send REPLCONF capa psync2
+    // const char *rp2 = "*3\r\n$8\r\nREPLCONF\r\n$4\r\ncapa\r\n$6\r\npsync2\r\n";
+    // if (send_all(fd, rp2, strlen(rp2)) != 0)
+    // {
+    //     printf("replica connect: send REPLCONF capa failed (%s)\n", strerror(errno));
+}
+// Send PSYNC ? -1
+// const char *ps = "*3\r\n$5\r\nPSYNC\r\n$1\r\n?\r\n$2\r\n-1\r\n";
+// if (send_all(fd, ps, strlen(ps)) != 0)
+// {
+//     printf("replica connect: send PSYNC failed (%s)\n", strerror(errno));
+// }
+// Store master coordinates on server for later stages
+strncpy(srv->master_host, host, sizeof(srv->master_host) - 1);
+srv->master_host[sizeof(srv->master_host) - 1] = '\0';
+srv->master_port = port;
+return 0;
 }
 
 void server_add_waiter(Server *srv, Conn *c, int fd, const char *key, size_t klen, int64_t deadline_ms)
